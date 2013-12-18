92b909d3b69003086cf3f5c32346811a9bdc9c46
yermilov <yermilov17@gmail.com>
2013-12-18 02:45:17 +0200

    add possibility to applies list

--- src/main/java/cyberwaste/brightonbeach/specification/ApplySpecifications.java
+++ src/main/java/cyberwaste/brightonbeach/specification/ApplySpecifications.java
@@ -5,13 +5,20 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.lang.StringUtils;
+
 import cyberwaste.brightonbeach.domain.Apply;
 import cyberwaste.brightonbeach.domain.Apply_;
 import cyberwaste.brightonbeach.domain.Card;
+import cyberwaste.brightonbeach.domain.Card_;
 
 public final class ApplySpecifications {
     
     private ApplySpecifications() { }
+    
+    public static SerializableSpecification<Apply> any() {
+        return SpecificationUtils.any();
+    }
     
     public static SerializableSpecification<Apply> ofCard(final Card card) {
         return new SerializableSpecification<Apply>() {
@@ -20,6 +27,30 @@ public final class ApplySpecifications {
             @Override
             public Predicate toPredicate(Root<Apply> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
                 return criteriaBuilder.equal(root.get(Apply_.card), card);
+            }
+        };
+    }
+    
+    public static SerializableSpecification<Apply> where(final Card exactCard, final String cardNameLike) {
+        return new SerializableSpecification<Apply>() {
+            
+            private static final long serialVersionUID = 1L;
+            
+            @Override
+            public Predicate toPredicate(Root<Apply> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                Predicate resultPredicate = criteriaBuilder.isTrue(criteriaBuilder.literal(Boolean.TRUE));
+                
+                if (exactCard != null) {
+                    root.join(Apply_.card);
+                    resultPredicate = criteriaBuilder.and(resultPredicate, criteriaBuilder.equal(root.get(Apply_.card), exactCard));
+                } else if (StringUtils.isNotBlank(cardNameLike)) {
+                    root.join(Apply_.card);
+                    resultPredicate = criteriaBuilder.and(resultPredicate,
+                        criteriaBuilder.like(criteriaBuilder.upper(root.get(Apply_.card).get(Card_.name)), SpecificationUtils.prepareSqlContainsPattern(cardNameLike))
+                    );
+                }
+                
+                return resultPredicate;
             }
         };
     }
